b0c7f9a9198f0255285d3ce99d9ef472e04e5527
Nethelbert <gilblasse@gmail.com>
2020-01-19 12:19:22 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,9 +11,9 @@ class StudentsController < ApplicationController
   def activate
     @student = Student.find(params[:id])
     if @student.active 
-      @student.active = false
-    else 
       @student.active = true
+    else 
+      @student.active = false
     end
   end
 
